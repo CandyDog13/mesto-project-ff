816a9005b9da2c363712e30bd3fa0f64675fa275
CandyDog13 <kga13@yandex.ru>
2024-02-18 21:56:12 +0300

    Создал добавление 6 карточек из массива и их удаление

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,28 @@
 // @todo: Темплейт карточки
+    const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+    const cardsContainer = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function addCard(nameCard, linkCard) {
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+    cardElement.querySelector('.card__image').src = linkCard;
+    cardElement.querySelector('.card__title').textContent = nameCard;
+    cardElement.querySelector('.card__delete-button').addEventListener('click', function(event) {
+        deleteCard(event.target);
+      })
+    console.log(cardElement);
+    cardsContainer.append(cardElement);
+  }
 
 // @todo: Функция удаления карточки
+function deleteCard(indexOfDelete) {
+    indexOfDelete.closest('.places__item').remove();
 
+}
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach(function(element){
+    return addCard(element.name, element.link);
+});
